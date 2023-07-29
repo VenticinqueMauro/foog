21c79b4aad1ba64c10fd2440e6a0d607f9dd2094
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-07-29 15:14:02 -0300

    agregado de texturas

--- src/app/components/galeria/categorias/CategoryContainer.tsx
+++ src/app/components/galeria/categorias/CategoryContainer.tsx
@@ -7,11 +7,11 @@ export default function CategoryContainer() {
     return (
         <div className="flex flex-col h-screen gap-2 overflow-y-hidden pt-28 ">
             <Title title={'galería de imagenes'} />
-            <div className="flex items-center justify-center flex-grow ">
+            <div className="flex items-center justify-center flex-grow">
                 {categoryItems.map((item, i) => (
                     <div
                         key={i}
-                        className="relative h-[80%] w-[300px] hover:w-[500px] opacity-60 hover:opacity-100 group transition-all duration-300 reflejo "
+                        className="relative h-[80%] w-[300px] hover:w-[500px] opacity-60 hover:opacity-100 group transition-all duration-300 reflejo"
                     >
                         <Image
                             src={item.img}
@@ -20,7 +20,7 @@ export default function CategoryContainer() {
                             alt="fotoCategory"
                             className="object-cover w-full h-full rounded "
                         />
-                        <p className="absolute top-0 left-0 flex items-end gap-4 p-3 mt-4 text-xl font-semibold capitalize group-hover:bg-black/70 group-hover:rounded-r-[6px] pr-8 cristal group-hover:border-[#d6fb41] group-hover:border-t-2 group-hover:border-b-2 group-hover:border-r-2 ">
+                        <p className="absolute top-0 left-0 flex items-end gap-4 p-3 mt-4 text-xl font-semibold capitalize group-hover:bg-black/70 group-hover:rounded-r-[6px] pr-8 group-hover:border-[#d6fb41] group-hover:border-t-2 group-hover:border-b-2 group-hover:border-r-2 ">
                             <b className="text-3xl text-[#d6fb41] typing-text">0{Number(i + 1)}</b> {item.name}
                         </p>
                         <ZoomButton categoryName={item.name} />
