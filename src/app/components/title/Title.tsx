a77a7a13ffcc301f064d52a308b447150d80d300
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-07-30 10:59:19 -0300

    agregado de sombra a los titulos

--- src/app/components/title/Title.tsx
+++ src/app/components/title/Title.tsx
@@ -1,12 +1,15 @@
 import { Roboto_Condensed, Sora } from "next/font/google"
 
 const robotoCondensed = Roboto_Condensed({ subsets: ['latin'], weight: '700' })
-const sora = Sora({ subsets: ['latin'], style: "normal", weight: "500"})
+const sora = Sora({ subsets: ['latin'], style: "normal", weight: "500" })
 
 
 export default function Title({ title }: { title: string }) {
 
     return (
-        <h1 className={`${robotoCondensed.className} text-4xl md:text-5xl lg:text-6xl font-bold text-center capitalize text-[#ACAA9E]`} style={{textShadow: "0px 0px 5px #161616"}}>{title}</h1>
+        <div className="relative">
+            <h1 className={`${robotoCondensed.className} text-4xl md:text-5xl lg:text-7xl font-bold text-center capitalize text-[#ACAA9E]`} style={{ textShadow: "0px 0px 5px #161616" }}>{title}</h1>
+            <span className="md:text-6xl lg:text-8xl font-bold  text-[#ACAA9E]/5 absolute bottom-0  text-center w-full capitalize">{title}</span>
+        </div>
     )
 }
