788426fc572135bd43b01dc59c74285a7b6fd202
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-08-05 18:22:14 -0300

    fix: solucionado error de cierre ventana de dropDown

--- src/app/components/dropDown/Dropdown.tsx
+++ src/app/components/dropDown/Dropdown.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { motion } from "framer-motion";
 import { Menu } from '@headlessui/react';
 import Link from 'next/link';
@@ -9,19 +7,18 @@ import { useState, useEffect } from 'react';
 import { useStore } from '@/app/store/store';
 
 export default function Dropdown({ liClass, handleClick }: { liClass: string, handleClick: () => void }) {
-
-
     const [isDpOpen, setIsDpOpen] = useState(false);
     const { setIsSidebarOpen } = useStore();
 
     const handleDropDown = () => {
-        setIsSidebarOpen()
-        setIsDpOpen((prev) => !prev)
-    }
+        setIsSidebarOpen();
+        setIsDpOpen((prev) => !prev);
+        console.log(isDpOpen);
+    };
 
     const closeDropdown = () => {
         setIsDpOpen(false);
-    }
+    };
 
     useEffect(() => {
         const handleOutsideClick = (event: MouseEvent | TouchEvent) => {
@@ -42,6 +39,10 @@ export default function Dropdown({ liClass, handleClick }: { liClass: string, ha
         };
     }, [isDpOpen]);
 
+    const handleMenuLinkClick = () => {
+        closeDropdown();
+        handleClick();
+    };
 
     return (
         <li className={`${liClass} relative`}>
@@ -53,20 +54,26 @@ export default function Dropdown({ liClass, handleClick }: { liClass: string, ha
                     Galería
                     <MdKeyboardArrowDown className={`${isDpOpen && 'transform rotate-180'} transition-transform duration-300`} />
                 </Menu.Button>
-                <Menu.Items className='absolute left-0 flex inline-flex flex-col gap-1 p-2 md:rounded top-10 mt-3 ml-3 border-l md:m-0 border-[#ACAA9E] md:border-none md:bg-[#25292B]/90 inline-block'>
-                    <Menu.Item >
-                        {({ active }) => (
-                            <Link
-                                href={`/galeria`}
-                                className={`${active && 'bg-[#161716] rounded '} hidden md:block capitalize p-2`}
-                                onClick={handleClick}
-                            >
-                                Principal
-                            </Link>
-                        )}
-                    </Menu.Item>
-                    {
-                        categoryItems.map((item, index) => (
+                {isDpOpen && (
+                    <motion.div
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -20 }}
+                        transition={{ duration: 0.3 }}
+                        className='dropdown-menu absolute left-0 flex inline-flex flex-col gap-1 p-2 md:rounded top-10 mt-3 ml-3 border-l md:m-0 border-[#ACAA9E] md:border-none md:bg-[#25292B]/90 inline-block'
+                    >
+                        <Menu.Item>
+                            {({ active }) => (
+                                <Link
+                                    href={`/galeria`}
+                                    className={`${active && 'bg-[#161716] rounded '} hidden md:block capitalize p-2`}
+                                    onClick={() => { handleMenuLinkClick(); }}
+                                >
+                                    Principal
+                                </Link>
+                            )}
+                        </Menu.Item>
+                        {categoryItems.map((item, index) => (
                             <Menu.Item key={item.name}>
                                 {({ active }) => (
                                     <motion.div
@@ -79,7 +86,7 @@ export default function Dropdown({ liClass, handleClick }: { liClass: string, ha
                                         <Link
                                             href={`/galeria/${item.href}`}
                                             className={`${active && 'bg-[#161716] rounded '} capitalize p-2 flex items-center gap-1`}
-                                            onClick={handleDropDown}
+                                            onClick={() => { handleMenuLinkClick(); }}
                                         >
                                             <span>{item.icon ?? null}</span>
                                             {item.name}
@@ -87,64 +94,10 @@ export default function Dropdown({ liClass, handleClick }: { liClass: string, ha
                                     </motion.div>
                                 )}
                             </Menu.Item>
-                        ))
-                    }
-                </Menu.Items>
+                        ))}
+                    </motion.div>
+                )}
             </Menu>
         </li>
     )
 }
-
-
-        // <li className={`${liClass} relative`}>
-        //     <Menu>
-        //         <Menu.Button
-        //             className='flex items-center justify-between w-full gap-2 md:justify-normal'
-        //             onClick={() => setIsDpOpen((prev) => !prev)}
-        //         >
-        //             Galería
-        //             <MdKeyboardArrowDown className={`${isDpOpen && 'transform rotate-180'} transition-transform duration-300`} />
-        //         </Menu.Button>
-        //         <AnimatePresence>
-        //             {isDpOpen && (
-        //                 <motion.div
-        //                     initial={{ opacity: 0, y: -20 }} // Posición inicial para aparecer desde arriba
-        //                     animate={{ opacity: 1, y: 0 }} // Posición final (totalmente visible)
-        //                     exit={{ opacity: 0, y: -20 }} // Animación al cerrar (desaparecer hacia arriba)
-        //                     transition={{ duration: 0.3 }} // Duración de la animación (ajusta según lo desees)
-        //                     className='dropdown-menu absolute left-0 flex inline-flex flex-col gap-1 p-2 md:rounded top-10 mt-3 ml-3 border-l md:m-0 border-[#ACAA9E] md:border-none md:bg-[#25292B]/90 inline-block'
-        //                 >
-        //                     <Menu.Item >
-        //                         {({ active }) => (
-        //                             <Link
-        //                                 href={`/galeria`}
-        //                                 className={`${active && 'bg-[#161716] rounded '} hidden md:block capitalize p-2`}
-        //                                 onClick={handleDropDown}
-        //                             >
-        //                                 Principal
-        //                             </Link>
-        //                         )}
-        //                     </Menu.Item>
-        //                     {
-        //                         categoryItems.map((item, index) => (
-        //                             <Menu.Item key={item.name}>
-        //                                 {({ active }) => (
-        //                                     <motion.div
-        //                                         initial={{ opacity: 0, x: -10 }} // Posición inicial para aparecer desde la izquierda
-        //                                         animate={{ opacity: 1, x: 0 }} // Posición final (totalmente visible)
-        //                                         exit={{ opacity: 0, x: -10 }} // Animación al cerrar (desaparecer hacia la izquierda)
-        //                                         transition={{ duration: 0.3, delay: index * 0.1 }} // Delay para escalonar los elementos
-        //                                         className={`${active && 'bg-[#161716] rounded '} capitalize p-2 flex items-center gap-1`}
-        //                                     >
-        //                                         <span>{item.icon ?? null}</span>
-        //                                         {item.name}
-        //                                     </motion.div>
-        //                                 )}
-        //                             </Menu.Item>
-        //                         ))
-        //                     }
-        //                 </motion.div>
-        //             )}
-        //         </AnimatePresence>
-        //     </Menu>
-        // </li>
--- src/app/components/loader/Loader.about.tsx
+++ src/app/components/loader/Loader.about.tsx
@@ -1,9 +1,15 @@
+const skeletonArray = Array.from({ length: 30 });
 
 export default function LoaderAbout() {
+
+    console.log(skeletonArray.length)
+
     return (
-        <span className="flex flex-col h-screen gap-10 animate-pulse">
-            <span className='block h-12 w-[250px] md:w-[400px] lg:w-[600px] mx-auto rounded rounded-xl bg-zinc-700'>&nbsp;</span>
-            <span className="block w-full h-full rounded bg-zinc-700"></span>
+        <span className="flex flex-col h-screen animate-pulse">
+            <span className='block h-6 md:h-10 w-[250px] md:w-[400px] lg:w-[600px] mx-auto rounded rounded-xl bg-zinc-700 mb-10'>&nbsp;</span>
+            {
+                skeletonArray.map((skelenton, i) => <span key={i} className="block w-full h-4 my-1 rounded md:my-2 rounded-xl bg-zinc-700">&nbsp;</span>)
+            }
         </span>
     )
 }
