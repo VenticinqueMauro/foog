b8f23f1ff62295e5717c5e29d806913535c31a51
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-08-17 20:03:59 -0300

    email funcionales con emailsJs

--- src/app/contacto/components/Form.tsx
+++ src/app/contacto/components/Form.tsx
@@ -1,20 +1,26 @@
 'use client'
 
+
+import { useRef } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { MdOutlineArrowRight } from 'react-icons/md';
 import { ZodType, z } from 'zod';
 import { Toaster, toast } from 'sonner';
+import emailjs from '@emailjs/browser';
 
 export type FormData = {
-    email: string;
-    consulta: string;
+    user_email: string;
+    message: string;
 }
 
 export default function Form() {
+
+    const form: any = useRef();
+
     const schema: ZodType<FormData> = z.object({
-        email: z.string().email({ message: "Dirección de correo electrónico inválida" }),
-        consulta: z.string().min(20, { message: "La consulta debe contener al menos 20 carácter(es)" }).max(500, { message: "La consulta debe contener un máximo de 500 caracteres" })
+        user_email: z.string().email({ message: "Dirección de correo electrónico inválida" }),
+        message: z.string().min(20, { message: "La consulta debe contener al menos 20 carácter(es)" }).max(500, { message: "La consulta debe contener un máximo de 500 caracteres" })
     });
 
 
@@ -22,15 +28,29 @@ export default function Form() {
         resolver: zodResolver(schema)
     });
 
-    const submitData = (data: FormData) => {
-        console.log('DATA: ', data);
-        toast.success('Email enviado con éxito!');
+    const submitData = () => {
+        toast.promise(
+            emailjs.sendForm('service_u8e10ad', 'template_ao7vv8s', form.current, 'NzdOOQuatMXE7PrQn'),
+            {
+                loading: 'Enviando correo...',
+                success: (result) => {
+                    console.log(result.text);
+                    return '¡Correo enviado con éxito!';
+                },
+                error: (error) => {
+                    console.log(error.text);
+                    return '¡Ups! Algo salió mal.';
+                },
+            }
+        );
         reset();
-    }
+    };
+
 
     return (
         <div className="w-full h-full">
             <form
+                ref={form} // corroborar type
                 className="flex flex-col max-w-xl p-5 mx-auto border rounded shadow md:p-10 border-zinc-800 rounded-2xl bg-black/50 cristal"
                 onSubmit={handleSubmit(submitData)}
             >
@@ -40,12 +60,12 @@ export default function Form() {
                     id="email"
                     placeholder="ejemplo@ejemplo.com"
                     className="p-1 mt-3 mb-3 text-white border rounded bg-[rgba(214,251,252,.057)] placeholder:px-1 placeholder:opacity-50 border-zinc-800"
-                    {...register("email")}
+                    {...register("user_email")}
                 />
                 {
-                    errors.email &&
+                    errors.user_email &&
                     <span className='p-1 text-red-600 rounded bg-red-400/20'>
-                        {errors.email.message}
+                        {errors.user_email.message}
                     </span>
                 }
                 <label htmlFor="consulta" className='mt-5 text-zinc-400'>¿En qué podemos ayudarte? </label>
@@ -53,12 +73,12 @@ export default function Form() {
                     id="consulta"
                     placeholder="Ingresa tu consulta aquí"
                     className="h-32 mt-3 p-1 mb-3 text-white border rounded resize-none bg-[rgba(214,251,252,.057)] placeholder:px-1 placeholder:opacity-50 border-zinc-800"
-                    {...register("consulta")}
+                    {...register("message")}
                 />
                 {
-                    errors.consulta &&
+                    errors.message &&
                     <span className='p-1 text-red-600 rounded bg-red-400/20'>
-                        {errors.consulta.message}
+                        {errors.message.message}
                     </span>
                 }
                 <button
@@ -68,7 +88,7 @@ export default function Form() {
                     Enviar
                     <MdOutlineArrowRight className='text-2xl' /></button>
             </form>
-            <Toaster />
+            <Toaster  />
         </div>
     )
 }
