b7783bb2902ce89f9fa8a32e1cc139d4e26aae09
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-09-14 09:05:55 -0300

    agreago de 1s al slider

--- src/components/slideHome/ImageSlider.tsx
+++ src/components/slideHome/ImageSlider.tsx
@@ -19,7 +19,7 @@ const ImageSlider = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 3000);
+        }, 4000);
 
         return () => {
             clearInterval(interval);
