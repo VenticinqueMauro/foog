411c7c02a666851c8ca83b5abe45a88bf3453d2f
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-09-19 10:06:28 -0300

    textura del mundo en pagina de contacto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ async function page() {
   return (
     <PageTransitions>
       {/* <ImageSlider /> */}
-      <div className="w-screen h-screen">
+      <div className="w-screen h-screen ">
         <video className="object-cover w-full h-full" autoPlay muted loop>
           <source src="/video.mp4" type="video/mp4" />
         </video>
