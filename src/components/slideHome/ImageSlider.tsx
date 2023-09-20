401a75a6d7d9248f48076e859e579781822a163f
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-09-20 19:29:45 -0300

    recommit

--- src/components/slideHome/ImageSlider.tsx
+++ src/components/slideHome/ImageSlider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useMediaQuery } from '@react-hook/media-query';
 
