e967aee73e7c2dcf938a1af5346a22419db9df9f
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-09-21 09:33:25 -0300

    cambioos en imageSlider

--- src/components/slideHome/ImageSlider.tsx
+++ src/components/slideHome/ImageSlider.tsx
@@ -9,6 +9,7 @@ import image3 from '../../../public/3.jpg';
 import image4 from '../../../public/4.jpg';
 import image5 from '../../../public/5.jpg';
 import image6 from '../../../public/6.jpg';
+import Image from 'next/image';
 
 const images = [image2, image3, image4, image5, image6];
 
@@ -44,11 +45,21 @@ const ImageSlider = () => {
                     }}
                 >
                     {images.map((image, index) => (
-                        <div
+                        // <div
+                        //     key={index}
+                        //     className="w-screen h-screen bg-center bg-cover"
+                        //     style={{ backgroundImage: `url(${image.src})` }}
+                        // ></div>
+                        <motion.img
                             key={index}
-                            className="w-screen h-screen bg-center bg-cover"
-                            style={{ backgroundImage: `url(${image.src})` }}
-                        ></div>
+                            src={image.src}
+                            alt={`Image ${index}`}
+                            className="object-cover w-screen h-screen"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 1 }}
+                        />
                     ))}
                 </motion.div>
             </AnimatePresence>
