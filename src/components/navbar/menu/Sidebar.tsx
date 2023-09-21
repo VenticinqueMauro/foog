c75216c496e70654c935b2adc08fbf8e73dcfd20
Venticinque Mauro <mauro.venticinque@hotmail.com>
2023-09-21 08:54:41 -0300

    cambio de bg en sidebar, mal funcionamiento

--- src/components/navbar/menu/Sidebar.tsx
+++ src/components/navbar/menu/Sidebar.tsx
@@ -34,7 +34,7 @@ export default function Sidebar() {
                 animate={{ width: isSidebarOpen ? 240 : 0, opacity: isSidebarOpen ? 1 : 0 }}
                 exit={{ width: 0, opacity: 0 }}
                 transition={{ duration: isSidebarOpen ? 0.1 : 0.3 }}
-                className={`fixed top-0 left-0 z-20 min-h-screen py-3 shadow-lg shadow-black bgSideBar`}
+                className={`fixed top-0 left-0 z-20 min-h-screen py-3 shadow-lg shadow-black bg-zinc-950`}
             >
                 <ContainerMenu ulClass={ulClassMobile} liClass={liClassMobile} />
             </motion.section>
