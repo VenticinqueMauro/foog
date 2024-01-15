76fea6516d7f1c08c01f4401098e693beee1c63f
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-01-15 18:42:11 -0300

    cambio de imagen logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../../public/logo.jpg"
+import logo from "../../../public/logo.png"
 import Link from "next/link";
 
 export default function Logo() {
